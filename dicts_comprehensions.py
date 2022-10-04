c67c1898afd24470dd1a15b9c0b17929efa422af
erick <kaulitz@hotmail.cl>
2022-10-04 17:00:02 -0300

    comprehensions

---
+++ dicts_comprehensions.py
@@ -0,0 +1,21 @@
+
+def run():
+    # new_dict = {}
+    
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         new_dict[i] = i **3
+        
+
+    # print(new_dict)
+
+    # my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    # print(my_dict)
+
+    # n_dict = {i: i**0.5 for i in range(1, 1001)}
+    # print(n_dict)
+    new_list = [ i * i for i in range(1, 20)]
+    print(new_list)
+
+if __name__ == "__main__":
+    run()
